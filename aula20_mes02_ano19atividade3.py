d29a70834a4ba7d61a18302cecf44a67d936f382
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-03-27 19:24:32 -0300

    Create aula20_mes02_ano19atividade3.py

---
+++ aula20_mes02_ano19atividade3.py
@@ -0,0 +1,12 @@
+# Toda entrada do input é STR (string)
+idade = input("Digite sua idade: ")
+# Posso converter de STR para INT
+# idade = int(idade)
+idade = float(idade)
+# print("Sua idade é", idade)
+# float - decimais
+# Calculando a metade
+metade = (idade / 2)
+
+print("A metade é", metade)
+# print("O tipo de variavel é", type(idade))
