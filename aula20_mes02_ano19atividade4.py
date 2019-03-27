27df9eeae9f762736a7ee82369635237b866086d
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-03-27 19:25:41 -0300

    Create aula20_mes02_ano19atividade4.py

---
+++ aula20_mes02_ano19atividade4.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+print("Valor de a: ", a)
+print("Valor de b: ", b)
+
+print("  ")
+print("=================")
+print("  ")
+c = a
+a = b
+b = c
+print("  ")
+
+print("Valor de a: ", a)
+print("Valor de b: ", b)
