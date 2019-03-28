a24ddb561449bb63267f86586ab2b57cf82dbd0e
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-03-27 22:34:16 -0300

    Exercícios para a prova

---
+++ aula27_mes03_ano19.py
@@ -0,0 +1,92 @@
+import math
+
+#print(math.sqrt(4))
+
+# x = 14.8
+# z = math.ceil(x)
+# print(math.ceil(z))
+
+# x = 14.8
+# z = math.floor(x)
+# print(math.floor(z))
+
+# x = 14.8
+# z = math.trunc(x)
+# print(math.trunc(z))
+
+# x = math.pow(5, 3)
+# print(x)
+
+#10
+
+# raio = float(input("Digite o valor de raio: "))
+# r = raio**2
+# pi = math.pi
+# área = pi * r
+#
+# print("A área do círculo é ", math.ceil(área), "metros quadrados!")
+
+#12
+
+# x = 10
+# y = 20
+# z = 30
+#
+# print("Os valores de x, y e z são ", x, y, z,)
+# print("  ")
+#
+# print("Porém os valores mudaram!")
+# a = x
+# x = z
+# z = y
+# y = a
+# print("  ")
+#
+# print("Agora são", x, y, z,)
+
+#13
+# x = 100219
+# ano = x % 100
+# x = int(x / 100)
+# mês = x % 100
+# data = int(x / 100)
+# dia = x
+# print(dia)
+
+
+
+
+#14
+
+# num1 = float(input("Digite um número: "))
+#
+# print("O valor é", num1 / 3)
+# num2 = num1 - (2/3) * num1
+#
+# print(num2)
+
+#16
+
+# angulo = float(input("Digite um ângulo: "))
+#
+# seno = math.sin(angulo)
+# cosseno = math.cos(angulo)
+# tangente = math.tan(angulo)
+#
+# print("O seno do ângulo é ", seno,)
+# print("O cosseno do ângulo é", cosseno,)
+# print("A tangente do ângulo é", tangente,)
+
+#17
+
+# salário = float(input("Digite o valor do salário minímo: "))
+# quilowatts = float("Valor do quilowatts: ")
+
+#20
+a = float(input("Digite o valor de A: "))
+b = float(input("Digite o valor de B: "))
+c = float(input("Digite o valor de C: "))
+
+x = b / (a + c)
+y = 2 * (a - b)
+z = 
