be6cca27cc1d0db5c4af08782b07faaf68f2947d
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-04-03 07:25:18 -0700

    Add files via upload

---
+++ algoritmos.py
@@ -0,0 +1,180 @@
+import math
+
+#print(math.sqrt(4))
+
+# x = 14.8
+# z = math.ceil(x)
+# print(math.ceil(z))
+
+# x = 14.8
+# z = math.floor(x)
+# print(math.floor(z))
+
+# x = 14.8
+# z = math.trunc(x)
+# print(math.trunc(z))
+
+# x = math.pow(5, 3)
+# print(x)
+
+#10
+
+# raio = float(input("Digite o valor de raio: "))
+# r = raio**2
+# pi = math.pi
+# área = pi * r
+#
+# print("A área do círculo é ", math.ceil(área), "metros quadrados!")
+
+#12
+
+# x = 10
+# y = 20
+# z = 30
+#
+# print("Os valores de x, y e z são ", x, y, z,)
+# print("  ")
+#
+# print("Porém os valores mudaram!")
+# a = x
+# x = z
+# z = y
+# y = a
+# print("  ")
+#
+# print("Agora são", x, y, z,)
+
+#13
+# x = 100219
+# ano = x % 100
+# x = int(x / 100)
+# mês = x % 100
+# data = int(x / 100)
+# dia = x
+# print(dia)
+
+
+
+
+#14
+
+# num1 = float(input("Digite um número: "))
+#
+# print("O valor é", num1 / 3)
+# num2 = num1 - (2/3) * num1
+#
+# print(num2)
+
+#16
+
+# angulo = float(input("Digite um ângulo: "))
+#
+# seno = math.sin(angulo)
+# cosseno = math.cos(angulo)
+# tangente = math.tan(angulo)
+#
+# print("O seno do ângulo é ", seno,)
+# print("O cosseno do ângulo é", cosseno,)
+# print("A tangente do ângulo é", tangente,)
+
+#17
+
+# salário = float(input("Digite o valor do salário minímo: "))
+# quilowatts = float("Valor do quilowatts: ")
+
+
+#18
+
+# nome = input("Digite seu nome: ")
+# print("  ")
+#
+# parte1 = nome
+# print("Seu nome completo é ", parte1,)
+#
+# parte2 = nome [0]
+# print("A primeira letra do seu nome é ", parte2,)
+#
+# parte3 = nome [-1]
+# print("A última letra do seu nome é ", parte3,)
+#
+# parte4 = nome [0:3]
+# print("Da primeira letra até a terceira é ", parte4,)
+#
+# parte5 = nome [3]
+# print("O quarto caractere é ", parte5,)
+#
+# parte6 = nome [1:]
+# print("Todos os caracteres, exceto o primeiro fica ", parte6,)
+#
+# parte7 = nome [-2:]
+# print("As duas últimas letras são ", parte7,)
+
+#19
+
+# a = float(input("Digite o valor de a: "))
+# b = float(input("Digite o valor de b: "))
+# c = float(input("Digite o valor de c: "))
+#
+# pi = math.pi
+# conta1 = b / (a + c)
+# conta2 = 2*(a - b)
+# conta3 = math.log2(64)
+# conta4 = pi * 3
+#
+# conta5 = conta1 + conta2 + conta3 / conta4
+#
+# conta6 = conta5 + 1
+#
+# print("O valor de x é ", conta6,)
+
+
+#20
+
+# tempo = float(input("Digite o tempo de viagem: "))
+# velocidade = float(input("Digite a velocidade média: "))
+# distancia = tempo * velocidade
+#
+# litrosusados = distancia / 12
+#
+# print("  ")
+# print("Com base no tempo e na velocidade média, irá percorrer ", distancia, "km e gastará ", math.ceil(litrosusados), "litros.")
+
+#22
+
+# nome = "Dino da Silva Sauro"
+# tamanho = len(nome)
+#
+# print("A mensagem ", nome, "possui", tamanho, "caracteres!")
+
+#23
+
+# a = int(input("Digite o valor de a: "))
+# b = int(input("Digite o valor de b: "))
+# c = int(input("Digite o valor de c: "))
+#
+# if (a <= (b + c)) and (b <= (a + c)) and (c <= (a + b)):
+#     print("Podem ser lados de um triângulo!")
+# else:
+#     print("Não podem ser lados de um triângulo!")
+
+#25
+
+# estado = input("Em qual estado você nasceu: ")
+#
+# if (estado == Paraná):
+#     print("Paranaense!")
+
+# nome = input("Nome: ")
+# nome2 = nome.flower()
+# print(nome2)
+
+# nome = input("Nome: ")
+# nome2 = nome.upper()
+# print(nome2)
+
+# sigla = input("Sigla do estado onde você nasceu: ")
+#
+# if (sigla == PR) or (sigla == Pr) or (sigla == pR) or (sigla == pr):
+#     print("Paranaense")
+# else:
+#     print("kviwfbjwf")
