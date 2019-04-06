1b5506dabaca28eb75a0de2b39a79507105efbed
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-04-06 20:01:41 -0300

    exercícios

---
+++ algoritmos2.py
@@ -0,0 +1,107 @@
+#1
+# num1 = int(input("Digite um número: "))
+#
+# if (num1 >= 0):
+#     print("Esse número é positivo!")
+# else:
+#     print("Esse número é negativo!")
+
+#2
+# x = int(input("Digite um número:"))
+# y = int(input("Digite outro número: "))
+#
+# if (x > y):
+#     print("O número ", x, "é maior!")
+# else:
+#     print("O número ", y, "é maior!")
+
+#3
+# num1 = int(input("Digite um número: "))
+# if (num1 %2 == 0):
+#     print("É par!")
+# else:
+#     print("É ímpar!")
+
+#4
+# num1 = int(input("Digite um número: "))
+# if (num1 %3 == 0):
+#     print("É múltiplo de três!")
+# else:
+#     print("Não é múltiplo de três!")
+
+#5
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite um número: "))
+#
+# if (num1 %num2 == 0):
+#     print("O primeiro é múltiplo do segundo!")
+# else:
+#     print("Não são múltiplos!")
+
+#6
+# sexo = input("Informe seu sexo pelas letras F (feminino), M (masculino) ou O (outros): ")
+# if (sexo == "F"):
+#     print("Sexo feminino!")
+# elif (sexo == "M"):
+#     print("Sexo masculino!")
+# else:
+#     print("Outros!")
+
+#7
+# a = int(input("Digite o valor de a: "))
+# b = int(input("Digite o valor de b: "))
+# c = int(input("Digite o valor de c: "))
+#
+# if (a + b > c):
+#     print("A soma de ", a, "e ", b, "é maior que ",c, "!")
+# else:
+#     print("A soma de ", a, "e ", b, "não é maior que ",c, "!")
+
+#8
+
+# num1 = int(input("Digite um número: "))
+#
+# if (num1 %2 == 0):
+#     conta1 = num1 + 5
+#     print("Por ser par, o número foi somado com cinco que tem resultado ", conta1,)
+# else:
+#     conta2 = num1 + 8
+#     print("Por ser ímpar, o número foi somado com oito que tem resultado ", conta2,)
+
+#9
+
+# trabalho = int(input("Digite a nota do trabalho: "))
+# prova = int(input("Digite a nota da prova: "))
+#
+# if ((trabalho + prova) / 2 >= 60):
+#     print("Aprovado!")
+# else:
+#     print("Reprovado!")
+
+#10
+
+# a = int(input("Digite o valor de a: "))
+# b = int(input("Digite o valor de b: "))
+# c = int(input("Digite o valor de c: "))
+#
+# if (a > b) and (a > c):
+#     print("O número", a, "é maior que todos!")
+# elif (b > a) and (b > c):
+#     print("O número", b, "é maior que todos!")
+# elif (c > a) and (c > b):
+#     print("O número", c, "é maior que todos!")
+# elif (a == b) and (a == c):
+#     print("São todos iguais!")
+# else:
+#     print("Inválido!")
+
+#12
+
+# num1 = int(input("Digite um número: "))
+# if (num1 >= 0):
+#     conta1 = num1 * 10
+#     print("O número multiplicado tem como resultado", conta1,)
+# else:
+#     print("Número inválido!")
+
+#13
