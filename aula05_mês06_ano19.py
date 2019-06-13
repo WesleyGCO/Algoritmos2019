42a0a4ba5ab0d9fbc02fae4ddaeddf2dd73b2676
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-06-12 22:17:04 -0300

    Update aula05_mês06_ano19.py
    Arrumado para o dia 12-06-19

--- aula05_mês06_ano19.py
+++ aula05_mês06_ano19.py
@@ -84,6 +84,85 @@ f) Mostre na tela a quantidade de números pares.
 
 
 #4
+# cont = 0
+# x = int(input("Digite o valor do vetor: "))
+#
+# v1 = [0]*x
+# for cont in range (0, len(v1)):
+#     v1 [cont] = int(input("Digite valores dentro do vetor: "))
+# cont = cont + 1
+# print(v1)
+
+#5
+# Apresente os números do vetor em ordem inversa.
+
+# for cont in range (len(v1), 0):
+#     cont = cont - 1
+# print(v1)
+
+# Apresente a soma de todos os elementos.
+
+# cont = 0
+# x = int(input("Digite o valor do vetor: "))
+# soma = 0
+# v1 = [0]*x
+# for cont in range (0, len(v1)):
+#     v1 [cont] = int(input("Digite valores dentro do vetor: "))
+#     soma = soma + v1 [cont]
+# cont = cont + 1
+# print(v1)
+# print("Soma: ", soma)
+
+# Calcule a média aritmética dos valores.
+
+# cont = 0
+# x = int(input("Digite o valor do vetor: "))
+# soma = 0
+#
+# v1 = [0]*x
+# for cont in range (0, len(v1)):
+#     v1 [cont] = int(input("Digite valores dentro do vetor: "))
+#     soma = soma + v1 [cont]
+# cont = cont + 1
+# print(v1)
+# print("Média dos números: ", soma / len(v1))
+
+# Liste na tela somente os números do vetor que estão em posições (índices) pares.
+
+# cont = 0
+# x = int(input("Digite o valor do vetor: "))
+#
+# v1 = [0]*x
+# for cont in range (0, len(v1)):
+#     v1 [cont] = int(input("Digite valores dentro do vetor: "))
+# cont = cont + 1
+# # print(v1)
+#
+# for cont in range (0, len(v1), 2):
+#     print(v1 [cont])
+
+#  Por exemplo: na sequência 5, 2, -2, -7, 3, 14, 10, -3, 9, -6, 4, 1 o usuário teria que informar 4 e 8 (posição inicial e final, respectivamente) para mostrar na tela somente os valores destacados.
+
+
+# cont = 0
+# x = int(input("Digite o valor do vetor: "))
+#
+# v1 = [0]*x
+# for cont in range (0, len(v1)):
+#     v1 [cont] = int(input("Digite valores dentro do vetor: "))
+# cont = cont + 1
+# print(v1)
+
+# cont = 0
+# v1 = int(input("Digite o valor de seu vetor: "))
+#
+# while (cont < len(v1)):
+#     print(vetor [cont])
+#     cont = cont + 1
+# print(vetor)
+
+"#################################################"
+
 cont = 0
 x = int(input("Digite o valor do vetor: "))
 
@@ -91,14 +170,67 @@ v1 = [0]*x
 for cont in range (0, len(v1)):
     v1 [cont] = int(input("Digite valores dentro do vetor: "))
 cont = cont + 1
-print(v1)
+# print(v1)
 
-#5
-# Apresente os números do vetor em ordem inversa.
+# soma = 0
+#
+# y = int(input("Digite o valor inicial: "))
+# z = int(input("Digite o valor final: "))
+#
+# for cont in range (y, z + 1):
+#     soma = soma + v1 [cont]
+# print("Soma: ", soma)
 
-for cont in range (len(v1), 0):
-    cont = cont - 1
-print(v1)
 
-# Apresente a soma de todos os elementos.
-# Calcule a média aritmética dos valores.
+# y = int(input("Digite o valor inicial: "))
+# # z = int(input("Digite o valor final: "))
+# maior = 0
+# menor = 0
+
+
+# maior = v1[0]
+# menor = v1[0]
+# pos = 0
+# posinc = pos
+# posifinal = pos
+# for cont in range (0, len(v1)):
+#     #if (cont == 0):
+#        # maior = v1[cont]
+#        # menor = v1[cont]
+#     if(maior < v1[cont]):
+#         maior = v1[cont]
+#         posinc = pos
+#
+#     elif(menor > v1[cont]):
+#         menor = v1[cont]
+#         posifinal = pos
+#     pos = pos + 1
+#
+# print("Maior: ", maior, "na posição ", posinc)
+# print("Menor: ", menor, "na posição ", posifinal)
+
+# cont = 0
+# par = 0
+# impar = 0
+# for cont in range (0, len(v1)):
+#     if (v1 [cont] %2 == 0):
+#         cont += 1
+#         par = par + 1
+#     else:
+#         cont += 1
+#         impar = impar + 1
+# print("Números pares: ", par)
+# print("Números ímpares: ", impar)
+
+# x = int(input("Digite o valor do vetor: "))
+# v1 = [0]*x
+# cont = 0
+# for cont in range (0, len (v1)):
+#     v1 [cont] = int(input("Digite um número: "))
+#     print(v1 [cont] * 2)
+#     cont = cont + 1
+
+tabuada = 1
+cont = 0
+
+while (cont < len(v1)):
