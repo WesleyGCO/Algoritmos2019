a7b1c3ec545d79e05bcb688bb8960201909c24ae
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-07-03 19:12:47 -0300

    Operadores lógicos

---
+++ mês3aula4_ano19.py
@@ -0,0 +1,13 @@
+# Operadores Lógicos
+"""
+(condição) and (condição)
+(condição) or (condição)
+not (condição)
+"""
+ x = 10
+ y = 20
+
+if (x < 50 and y < 30):
+     print("A condição é verdadeira!")
+else:
+    print("A condição é falsa!")
