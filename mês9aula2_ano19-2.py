3ef3be418e2d67006759afeaf98bdd10cdd456d9
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-09-18 22:21:49 -0300

    Funções dos exercícios (extensão)

---
+++ mês9aula2_ano19-2.py
@@ -0,0 +1,66 @@
+#Exercício 1
+
+# def mostrarNumero():
+#     num = int(input("Informe um número: "))
+
+#     return num
+
+
+#Exercício 2
+
+# import random
+
+# def importarNumero(num):
+#     aleatorio = random.uniform(1, num)
+
+#     return aleatorio
+
+
+#Exercício 3
+
+# def mes(x):
+#     if (x == 1):
+#         mes = ("Janeiro")
+#     elif (x == 2):
+#         mes = ("Fevereiro")
+#     elif (x == 3):
+#         mes = ("Março")
+#     elif (x == 4):
+#         mes = ("Abril")
+#     elif (x == 5):
+#         mes = ("Maio")
+#     elif (x == 6):
+#         mes = ("Junho")
+#     elif (x == 7):
+#         mes = ("Julho")
+#     elif (x == 8):
+#         mes = ("Agosto")
+#     elif (x == 9):
+#         mes = ("Setembro")
+#     elif (x == 10):
+#         mes = ("Outubro")
+#     elif (x == 11):
+#         mes = ("Novembro")
+#     elif (x == 12):
+#         mes = ("Dezembro")
+#     else:
+#         mes = ("mes inválido!")
+#     return mes
+
+
+#Exercício 4 (a)
+
+# def quadrado():
+#     a = int(input("Digite o valor do lado: "))
+#     resultado = a * a
+#     return resultado
+
+#Exercício 4 (b)
+
+def retangulo():
+    b = int(input("Digite o valor da base: "))
+    h = int(input("Digite o valor da altura: "))
+    resultado = b * h
+    return resultado
+
+
