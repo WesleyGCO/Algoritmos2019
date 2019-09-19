4a4604ca1a965d39988d20568d6259aafb46d30a
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-09-18 21:34:25 -0300

    Construindo funções

---
+++ mês9aula1_ano19.py
@@ -0,0 +1,22 @@
+# def obterNumero():
+#     num1 = int(input("Informe um número: "))
+#     return num1
+
+# x = obterNumero()
+
+# print(x)
+
+
+###########################################
+
+def maior(a, b):
+    if (a > b):
+        maior = a
+    else:
+        maior = b
+
+    return maior
+
+valor = maior(154, 154.1)
+
+print(valor)
