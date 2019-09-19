811b3b8ea514b81a855bcf8332ed41f4721e9b69
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-09-18 22:21:17 -0300

    Principal dos exercícios

---
+++ mês9aula2_ano19.py
@@ -0,0 +1,19 @@
+import funcoes
+
+#Execício 1
+# x = print(funcoes.mostrarNumero())
+
+#Exercício 2
+# valor = funcoes.importarNumero(45)
+# print(valor)
+
+#Exercício 3
+# m = funcoes.mes(12)
+# print(m)
+
+#Exercício 4 (a)
+# x = funcoes.quadrado()
+# print(x)
+
+#Exercício 4 (b)
+x = funcoes.retangulo
