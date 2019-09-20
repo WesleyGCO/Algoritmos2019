95a89ff9d8ef20e2feba72b57436da0a92632c7b
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-09-20 20:48:39 -0300

    Principal dos exercícios

--- mês9aula2_ano19-1.py
+++ mês9aula2_ano19-1.py
@@ -16,4 +16,32 @@ import funcoes
 # print(x)
 
 #Exercício 4 (b)
-x = funcoes.retangulo
+# x = funcoes.retangulo()
+# print(x)
+
+#Exercício 4 (c)
+# x = funcoes.triangulo()
+# print(x)
+
+#Exercício 4 (d)
+# x = funcoes.trapezio()
+# print(x)
+
+#Exercício 5
+# x = funcoes.fatorial(5)
+# print(x)
+
+#Exercício 6
+
+#Exercício 7
+v1 = [90, 986, 435, 231, 543, 675, 1002, 985]
+# x = funcoes.maior(v1)
+# print(x)
+
+#Exercício 8
+# x = funcoes.menor(v1)
+# print(x)
+
+#Exercício 9
+# x = funcoes.mediaVetor(v1)
+# print(x)
