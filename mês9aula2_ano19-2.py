afccf8dacbc5d45c39b557229237f11fe06bb744
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-09-20 20:49:33 -0300

    Funções dos exercícios (extensão)

--- mês9aula2_ano19-2.py
+++ mês9aula2_ano19-2.py
@@ -5,6 +5,7 @@
 
 #     return num
 
+# ============================================
 
 #Exercício 2
 
@@ -15,6 +16,7 @@
 
 #     return aleatorio
 
+# =============================================
 
 #Exercício 3
 
@@ -47,6 +49,7 @@
 #         mes = ("mes inválido!")
 #     return mes
 
+# ================================================
 
 #Exercício 4 (a)
 
@@ -57,10 +60,89 @@
 
 #Exercício 4 (b)
 
-def retangulo():
-    b = int(input("Digite o valor da base: "))
-    h = int(input("Digite o valor da altura: "))
-    resultado = b * h
-    return resultado
+# def retangulo():
+#     b = int(input("Digite o valor da base: "))
+#     h = int(input("Digite o valor da altura: "))
+#     resultado = b * h
+#     return resultado
 
+#Exercício 4 (c)
+
+# def triangulo():
+#     b = int(input("Digite o valor da base: "))
+#     h = int(input("Digite o valor da altura: "))
+#     resultado = (b *h)/2
+#     return resultado
+
+#Exercício 4 (d)
+
+# def trapezio():
+#     B = int(input("Digite o valor da base maior: "))
+#     h = int(input("Digite o valor da altura: "))
+#     b = int(input("Digite o valor da base menor: "))
+#     resultado = ((B * b) * h)/2
+#     return resultado
+
+# ================================================
+
+#Exercício 5
+
+# def fatorial(a):
+#     fat = 1
+#     i = 2
+#     while (i <= a):
+#         fat = fat * i
+#         i = i + 1
+#     resultado = fat
+#     return fat
+
+# ================================================
+
+#Exercício 6
+
+# def fatorialSeg(a):
+
+# ================================================
+
+#Exercício 7
+
+# def maior(a):
+#     maior = v1[0]
+#     for cont in range(0, len(a)):
+#         if (maior < a[cont]):
+#             maior = a[cont]
+#     return maior
+
+# ================================================
+
+#Exercício 8
+# def menor(a):
+#     menor = a[0]
+#     for cont in range(0, len(a)):
+#         if (menor > a[cont]):
+#            menor = a[cont]
+#     return menor 
+
+# ================================================
+
+#Exercício 9
+
+# def mediaVetor(a):
+#     soma = 0
+#     for cont in range(0, len(a)):
+#         soma = soma + a[cont]
+#     media = soma / len(a)
+#     return media
+
+# ================================================
+
+#Exercício 10
+
+def dobro(a):
+    n = 0
+    v2 = [0]*8
+    for cont in range(0, len(a)):
+        dobro {}.format(n + 1) = v1[cont] * 2
+        n = n + 1
+    for cont in range(0, le)
 
