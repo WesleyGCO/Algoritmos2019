a1248e5ab5cf36f5e9e49a0e5d4b8caac1117b7a
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-10-02 22:09:22 -0300

    Principal exercícios (arquivo)

---
+++ mês9aula3_ano19-1.py
@@ -0,0 +1,34 @@
+"""
+
+ARQUIVOS: conjunto de dados armazenados em disco.
+Ações:
+- Abrir arquivo
+    open("nomearquivo.txt", modoAbertura)
+        modos:  "r" - read (apenas lê dados do arquivo)
+                "w" - write (sempre "cria" um arquivo novo. Se já existir, ele é "zerado")
+                "a" - append (escreve no final de arquivo já existente)
+- Fechar arquivo
+- Gravar dados no arquivo
+"""
+
+# arquivo = open("nomes.txt", "w")
+# nome = input("Digite o nome: ")
+# arquivo.write(nome)
+# arquivo.write("\n")
+# arquivo.close()
+
+# ==============================================
+
+import arquivo
+
+#Exercício 1
+# arquivo.escrever_nome("nomes1.txt")
+
+#Exercício 2
+# arquivo.ler_arquivo("nomes1.txt")
+
+#Exercício 3
+arquivo.dados("dados3.txt")
+
+#Exercício 4
+# arquivo.ler_dados("dados3.txt")
