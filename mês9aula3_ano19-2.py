34500e24f9f255b1875a2a4fc6720ea861ab6efa
WesleyGCO <47651529+WesleyGCO@users.noreply.github.com>
2019-10-23 22:02:50 -0300

    Funções exercícios (extensão - arquivo)

--- mês9aula3_ano19-2.py
+++ mês9aula3_ano19-2.py
@@ -1,4 +1,7 @@
+
 #Exercício 1
+
+
 def escrever_nome(nomeArquivo):
     arquivo = open(nomeArquivo, "a")
     arquivo.write(input("Digite um nome: ") + "\n")
@@ -7,35 +10,96 @@ def escrever_nome(nomeArquivo):
 # ===============================================
 
 #Exercício 2
+
+
 def ler_arquivo(arq):
     arquivo = open(arq, "r")
     conteudo = arquivo.readlines()
 
     arquivo.close()
     for lin in range(0, len(conteudo)):
-        print(conteudo[lin], end = "")
+        print(conteudo[lin], end="")
 
 # ===============================================
 
 #Exercício 3
+
+
 def dados(x):
     y = open(x, "a")
     nome = input("Digite seu nome: ")
     email = input("Digite seu email: ")
     salario = input("Digite seu salário: ")
 
-    # y.write(nome + "\t" + email + "\t" + salario + "\t")
-    
+    y.write(nome + "||" + email + "||" + salario + "\n")
+
     y.close()
 
 # ===============================================
 
 #Exercício 4
+
+
 def ler_dados(x):
     y = open(x, "r")
     conteudo = y.readlines()
     y.close()
 
-    for lin in range(0, len(conteudo)):
-        print("Nome:", conteudo[lin], end = "")
+    for t in range(0, len(conteudo)):
+        d1 = conteudo[t].split("||")
+
+        print("Nome: ", d1[0], "\n")
+        print("Email: ", d1[1], "\n")
+        print("Salário: ", d1[2], "\n")
+
+# ===============================================
+
+#Exercício 5 (A)
+
+def ler_num(x):
+    y = open(x, "r")
+    conteudo = y.readlines()
+    y.close()
+    maior = float(conteudo[0])
+
+    for t in range(0, len(conteudo)):
+        if (maior < float(conteudo[t])):
+            maior = float(conteudo[t])
+    print("Maior: ", maior)
+
+#Exercício 5 (B)
+
+def num_maior(x):
+    y = open(x, "r")
+    conteudo = y.readlines()
+    y.close()
+    maiores = 0
+
+    for i in range(0, len(conteudo)):
+        if (float(conteudo[i]) > 0.05):
+            maiores = maiores + 1
+    print("Maiores que 0.05: ", maiores)
+
+#Exercício 5 (C)
+
+def quant_num(x):
+    y = open(x, "r")
+    conteudo = y.readlines()
+    y.close()
+
+    print("Quantidade de números: ", len(conteudo))
+
+# ===============================================
+
+#Exercício 6 (A)
+
+
+def ler_num2(x):
+    y = open(x, "r")
+    conteudo = y.readlines()
+    y.close()
+    
+    maior = float(conteudo[0])
+
+    for i in range()
         
