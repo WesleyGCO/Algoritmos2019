fb96d5d510114f9f4fdf4396a032952144f122d9
Wesley Castilho <47651529+WesleyGCO@users.noreply.github.com>
2019-11-06 21:55:27 -0300

    Estrutura de dados heterogênea (exemplos)

--- mês11aula1_ano2019.py
+++ mês11aula1_ano2019.py
@@ -1,1 +1,44 @@
+#--------------- Estrutura de dados heterogênea ------------------------------------
 
+# Em uma estrutura de dados heterogênea existem campos (atributos) e não índices;
+
+# Há registros (conteúdo de dados) - Class;
+
+# Nome ---> Atributos ---> *Métodos*;
+
+# O que é? é uma estrutura de dados que pode ter vários valores diferentes,
+# não sendo necessariamente do mesmo tipo (integer, float, etc);
+
+# Class é um projeto que constituem em atributos e métodos;
+
+# -----------------------------------------------------------------------------------
+import funcoes
+
+from Aluno import Aluno
+
+from Data import Data
+
+# -----------------------------------------------------------------------------------
+
+alu1 = Aluno()
+
+funcoes.exibirAluno(alu1)
+
+alu1.nome = "Flávio"
+alu1.idade = 19
+alu1.status = True
+alu1.dtNasc.dia = 6
+alu1.dtNasc.mes = 6
+alu1.dtNasc.ano = 2000
+alu1.dtNasc.hor.horas = 19
+alu1.dtNasc.hor.minutos = 20
+alu1.dtNasc.hor.segundos = 00
+
+funcoes.exibirAluno(alu1)
+
+# -----------------------------------------------------------------------------------
+
+# dtNasc = Data()
+# funcoes.informeData(dtNasc)
+
+# -----------------------------------------------------------------------------------
